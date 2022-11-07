541b621a12fab88c9f2f0fdd7016fd2f81e886d4
DOSBOL ALIEV <DosbolAliev21@gmail.com>
2022-11-06 21:25:34 -0600

    new feature

--- frontend_1/src/admin/AddCategory.js
+++ frontend_1/src/admin/AddCategory.js
@@ -1,32 +1,33 @@
-import React, { useState } from 'react';
-import Layout from '../core/Layout';
-import { isAuthenticated } from '../auth';
-import { Link } from 'react-router-dom';
-import { createCategory } from './apiAdmin';
+import React, { useState } from "react";
+import Layout from "../core/Layout";
+import { isAuthenticated } from "../auth";
+import { Link } from "react-router-dom";
+import { createCategory } from "./apiAdmin";
 
 const AddCategory = () => {
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
 
+  // new feature by dosbol
   // destructure user and token from localstorage
   const { user, token } = isAuthenticated();
 
   const handleChange = (e) => {
-    setError('');
+    setError("");
     setName(e.target.value);
   };
 
   const clickSubmit = (e) => {
     e.preventDefault();
-    setError('');
+    setError("");
     setSuccess(false);
     // make request to api to create category
     createCategory(user._id, token, { name }).then((data) => {
       if (data.error) {
         setError(data.error);
       } else {
-        setError('');
+        setError("");
         setSuccess(true);
       }
     });
@@ -34,36 +35,36 @@ const AddCategory = () => {
 
   const newCategoryForm = () => (
     <form onSubmit={clickSubmit}>
-      <div className='form-group'>
-        <label className='text-muted'>Name</label>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
         <input
-          type='text'
-          className='form-control'
+          type="text"
+          className="form-control"
           onChange={handleChange}
           value={name}
           autoFocus
           required
         />
       </div>
-      <button className='btn btn-outline-primary'>Create Category</button>
+      <button className="btn btn-outline-primary">Create Category</button>
     </form>
   );
 
   const showSuccess = () => {
     if (success) {
-      return <h3 className='text-success'>{name} is created</h3>;
+      return <h3 className="text-success">{name} is created</h3>;
     }
   };
 
   const showError = () => {
     if (error) {
-      return <h3 className='text-danger'>Category should be unique</h3>;
+      return <h3 className="text-danger">Category should be unique</h3>;
     }
   };
 
   const goBack = () => (
-    <div className='mt-5'>
-      <Link to='/admin/dashboard' className='text-warning'>
+    <div className="mt-5">
+      <Link to="/admin/dashboard" className="text-warning">
         Back to Dashboard
       </Link>
     </div>
@@ -71,11 +72,11 @@ const AddCategory = () => {
 
   return (
     <Layout
-      title='Add a new category'
+      title="Add a new category"
       description={`Hey ${user.name}, ready to add a new category?`}
     >
-      <div className='row'>
-        <div className='col-md-8 offset-md-2'>
+      <div className="row">
+        <div className="col-md-8 offset-md-2">
           {showSuccess()}
           {showError()}
           {newCategoryForm()}
